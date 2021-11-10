ee9c73fa772ae09923447aa6a1da5a67f7c8eba5
tunahan dursun <tunahandursun@hotmail.com>
2021-11-10 11:20:28 +0300

    bug fix

--- app.py
+++ app.py
@@ -29,15 +29,29 @@ def get_delta_hours(match):
     return delta_hours
 
 
+def get_teams_id_of_match(match):
+    home_team = match.get("homeTeam")
+    home_team_id = None if home_team is None else home_team.get("id")
+
+    away_team = match.get("awayTeam")
+    away_team_id = None if away_team is None else away_team.get("id")
+
+    return home_team_id, away_team_id
+
+
 @app.route("/check_matches")
 def check_incoming_matches():
     matches = get_matches()
     
+    result = MATCH_NOT_EXIST
     for match in matches:
         delta_hours = get_delta_hours(match)
-        if delta_hours < HOURS_INTERVAL["feature"] or delta_hours > HOURS_INTERVAL["past"]*-1:
-            if match.get("homeTeam") in BIG_TEAMS or match.get("awayTeam") in BIG_TEAMS:
-                app.logger.info("Crusial match found!")
+        if delta_hours < HOURS_INTERVAL["feature"] and delta_hours > HOURS_INTERVAL["past"]*-1:
+            home_team_id, away_team_id = get_teams_id_of_match(match)
+            
+            if home_team_id in BIG_TEAMS or away_team_id in BIG_TEAMS:
+                app.logger.info("Crusial match found! Match id: %d" %match.get("id"))
                 return CRUCIAL_MATCH_EXIST
-            return MATCH_EXIST
-    return MATCH_NOT_EXIST
+            app.logger.info("Match found! Match id: %s" %match.get("id"))
+            result = MATCH_EXIST
+    return result
